ae1e5b6a0cd0d5093c9b5dc2d6b1eb98726021f3
Samiksha pol <samikshapol1823@gmail.com>
2024-11-22 10:12:25 +0530

    second commit

--- src/xyz/Pattern.java
+++ src/xyz/Pattern.java
@@ -2,8 +2,8 @@ package xyz;
 
 public class Pattern {
 	public static void main(String[] args) {
-		for(int i=1;i<=4;i++) {
-			for(int j=1;j<=4;j++) {
+		for(int i=1;i<=3;i++) {
+			for(int j=1;j<=i;j++) {
 				System.out.print("*");
 			}
 			System.out.println(   );
